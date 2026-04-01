8f1657d7b22376893963888cceca089b5dfa9f0f
buloz <lbizeray30@gmail.com>
2026-04-01 02:39:54 +0200

    [ci] wrong workload

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -1,28 +1,28 @@
-#!bin/bash
+#!/bin/bash
 set -e
 
 BUILD_CONFIG="${BUILD_CONFIG:-Release}"
 ARTIFACTS_DIR="${ARTIFACTS_DIR:-/artifacts}"
-WORKSPACE="/workspace"
+SLN="${SLN:-src/MauiBlazorApp/MauiBlazorApp.slnx}"
 
 echo "Restore..."
-dotnet restore
+dotnet restore "$SLN"
 
 echo "Build..."
-dotnet build --no-restore -c $BUILD_CONFIG > build.log
-cat build.log
+dotnet build "$SLN" \
+    --no-restore \
+    -c $BUILD_CONFIG
+# cat build.log
 
 echo "Test..."
-dotnet test \
+dotnet test "$SLN" \
     --no-build \
     -c $BUILD_CONFIG \
-    --logger "trx;LogFileName=tests_results.trx" \
-    --logger "console;verbosity=normal" \
-    --results-directory $ARTIFACTS_DIR 
-cat tests_results.trx
+    --logger "trx;LogFileName=test_results.trx" \
+    --results-directory $ARTIFACTS_DIR
 
 echo "Collect artifacts..."
 mkdir -p $ARTIFACTS_DIR
-cp -r $WORKSPACE/artifacts/* $ARTIFACTS_DIR/ || true
+cp build.log $ARTIFACTS_DIR/ || true
 
 echo "Done."
